7ccd406f24a69d56619bb0b6c86807242d251d6e
jj-link <joseph.j.link@gmail.com>
2026-09-04 10:58:14 -0700

    Decode review few-shot files as UTF-8

--- ai_scientist/perform_llm_review.py
+++ ai_scientist/perform_llm_review.py
@@ -289,7 +289,7 @@ def load_paper(pdf_path, num_pages=None, min_size=100):
 
 
 def load_review(json_path):
-    with open(json_path, "r") as json_file:
+    with open(json_path, "r", encoding="utf-8") as json_file:
         loaded = json.load(json_file)
     return loaded["review"]
 
@@ -319,7 +319,7 @@ Note that while each review is formatted differently according to each reviewer'
     ):
         txt_path = paper_path.replace(".pdf", ".txt")
         if os.path.exists(txt_path):
-            with open(txt_path, "r") as f:
+            with open(txt_path, "r", encoding="utf-8") as f:
                 paper_text = f.read()
         else:
             paper_text = load_paper(paper_path)
